7ebb876f87bf4c99b5d2c02157761c1a52cdd4cf
JosepLlimona <62258739+JosepLlimona@users.noreply.github.com>
2025-01-21 19:38:39 +0100

    Coses

--- Assets/Scripts/NormalEnemy.cs
+++ Assets/Scripts/NormalEnemy.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.Rendering.VirtualTexturing.Debugging;
 
 public class NormalEnemy : MonoBehaviour
 {
@@ -13,10 +14,14 @@ public class NormalEnemy : MonoBehaviour
 
     private Animator swordAnimator; // Reference to the sword's Animator
 
+    private Rigidbody rb;
+    private bool canDie = false;
+
     void Start()
     {
         // Get the Animator component from the child object "sword"
         swordAnimator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
@@ -30,23 +35,44 @@ public class NormalEnemy : MonoBehaviour
             if (distanceToPlayer > stoppingDistance)
             {
                 Vector3 direction = (player.position - transform.position).normalized;
-                transform.position += direction * speed * Time.deltaTime;
+                rb.MovePosition(transform.position + direction * speed * Time.deltaTime);
 
                 // Rotate to face the player
                 Quaternion lookRotation = Quaternion.LookRotation(direction);
-                transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
-                swordAnimator.SetBool("IsAttacking", false);
+                rb.MoveRotation(Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed));
+                //swordAnimator.SetBool("IsAttacking", false);
             }
             else
             {
                 isAttacking = true;
                 // Trigger the sword's attack animation when close to the player
-                swordAnimator.SetBool("IsAttacking", true);
+                //swordAnimator.SetBool("IsAttacking", true);
             }
         }
     }
     void StopAttack()
     {
         isAttacking = false;
+    }
+
+    public void changeDie()
+    {
+        canDie = true;
+        StartCoroutine("revive");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (canDie && collision.gameObject.tag == "Katana")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private IEnumerator revive()
+    {
+        yield return new WaitForSeconds(2);
+        canDie = true;
+        transform.GetChild(0).GetComponent<EnemyController>().revive();
     }
 }
