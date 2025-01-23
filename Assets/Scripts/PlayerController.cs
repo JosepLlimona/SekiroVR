4fda0f59265e927449397a870317fb74a2b6a9e3
JosepLlimona <62258739+JosepLlimona@users.noreply.github.com>
2025-01-23 18:14:16 +0100

    Coses

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class PlayerController : MonoBehaviour
 {
 
     int potions = 3;
     int health = 100;
+
+    [SerializeField] ContinuousMoveProviderBase cmp;
+    [SerializeField] ContinuousTurnProviderBase ctp;
+    [SerializeField] TeleportationProvider tpp;
+    [SerializeField] SnapTurnProviderBase stp;
 
 
     public void GetHurt(int hurt)
@@ -44,5 +50,23 @@ public class PlayerController : MonoBehaviour
         {
             GetHurt(5);
         }
+    }
+
+    public void changeToTP()
+    {
+        Debug.Log("Canviant");
+        cmp.enabled = false;
+        ctp.enabled = false;
+        tpp.enabled = true;
+        stp.enabled = true;
+    }
+
+    public void changeToCont()
+    {
+        Debug.Log("Canviant");
+        cmp.enabled = true;
+        ctp.enabled = true;
+        tpp.enabled = false;
+        stp.enabled = false;
     }
 }
